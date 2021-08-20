ef82e03b8844a7b3479bd5c44ec0b23212239c47
Ashis Bhowmik <ashisbhowmikisp@gmail.com>
2021-08-20 11:20:13 +0530

    axios.js modified

--- src/axios.js
+++ src/axios.js
@@ -4,4 +4,5 @@ const instance = axios.create({
 });
 export default instance;
 
+// this is the access url tokenn
 // https://api.themoviedb.org/3/trending/all/week?api_key=b121aaf640101253ac097ec790655f99&language=en-US
